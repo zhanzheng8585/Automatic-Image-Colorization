505b5b9abcbd8abb0abf63030837624f693d473c
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-15 17:08:54 -0500

    Update run_res18.sh

--- run_res18.sh
+++ run_res18.sh
@@ -1,6 +1,7 @@
 export CUDA_VISIBLE_DEVICES=6
 
 python -u main.py          /home/zhanzheng/places365_standard \
+                           --epochs 100 \
                            --resume pretrained/checkpoint-trained.pth.tar &&
 echo "Congratus! Finished *color* training!"
 
