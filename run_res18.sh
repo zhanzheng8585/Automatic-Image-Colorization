f2b7a87d00f2f8d1b3816c34cfc7bf7248671e6a
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-15 16:18:06 -0500

    Update run_res18.sh

--- run_res18.sh
+++ run_res18.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=6
 
-python -u main8.py         data /home/zhanzheng/places365_standard &&
+python -u main.py         data /home/zhanzheng/places365_standard &&
 echo "Congratus! Finished *c8pattern* admm training!"
 
