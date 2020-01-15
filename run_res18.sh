7da6b9930221b2d019693691bf03a37e342a4d7f
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-15 16:29:44 -0500

    Update run_res18.sh

--- run_res18.sh
+++ run_res18.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=6
 
 python -u main.py          /home/zhanzheng/places365_standard \
-                           --pretrained pretrained/resnet_gray_weights.pth.tar &&
+                           --resume pretrained/checkpoint-trained.pth.tar &&
 echo "Congratus! Finished *c8pattern* admm training!"
 
