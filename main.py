c81f57bab40c822340ba8e8d0fe1a254227cc3f5
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-16 14:14:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,12 @@ parser.add_argument('--gpu', default=None, type=int,
                     help='GPU id to use.')
 parser.add_argument('--world-size', default=-1, type=int,
                     help='number of nodes for distributed training')
+parser.add_argument('--rank', default=-1, type=int,
+                    help='node rank for distributed training')
 parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
                     help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='nccl', type=str,
+                    help='distributed backend')
 parser.add_argument('--multiprocessing-distributed', action='store_true',
                     help='Use multi-processing distributed training to launch '
                          'N processes per node, which has N GPUs. This is the '
@@ -138,7 +142,7 @@ def main_worker(gpu, ngpus_per_node, args):
         dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
                                 world_size=args.world_size, rank=args.rank)
 
-    print("=> creating model '{}{}'".format(args.arch, args.depth))
+    # print("=> creating model '{}{}'".format(args.arch, args.depth))
 
     if args.distributed:
         # For multiprocessing distributed, DistributedDataParallel constructor
