1c65da656f12e0cf4e8116f76eea90ab49567fd3
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-16 14:18:51 -0500

    Update run_res18.sh

--- run_res18.sh
+++ run_res18.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=6,7
 python -u main.py           /home/zhanzheng/places365_standard \
 							--batch-size 128 \
 							-j 16 \
-							--lr 0.0001 \
+							--lr 0.001 \
                             --epochs 100 &&
 echo "Congratus! Finished *color* training!"
 
