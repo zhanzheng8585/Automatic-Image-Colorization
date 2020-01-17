6f8b4bd19e27dc60e2bcb46250dcc79b4225da46
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-17 12:51:46 -0500

    v1

--- run_res18.sh
+++ run_res18.sh
@@ -1,10 +1,9 @@
 export CUDA_VISIBLE_DEVICES=6,7
 
 python -u main.py           /home/zhanzheng/places365_standard \
-							--resume /home/zhanzheng/Automatic-Image-Colorization/checkpoints/model_best.pth.tar \
 							--batch-size 128 \
 							-j 16 \
-							--optmzr adam \
+							--optmzr sgd \
 							--lr 0.001 \
                             --epochs 100 &&
 echo "Congratus! Finished *color* training!"
