4f43213c0bfc9ffb90d44a731ae8b066be9f4812
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-17 14:14:58 -0500

    Update run_res18.sh

--- run_res18.sh
+++ run_res18.sh
@@ -7,7 +7,7 @@ python -u main.py           /home/zhanzheng/places365_standard \
 							--lr 0.001 \
 							--warmup \
 					   		--warmup-epochs 4 \
-							--no-tricks \
+							# --no-tricks \
                             --epochs 100 &&
 echo "Congratus! Finished *color* training!"
 
