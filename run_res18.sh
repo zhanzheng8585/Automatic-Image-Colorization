9236888e4707c6bdee5dbde9c8021b6bb1d60730
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-18 02:02:59 -0500

    Update run_res18.sh

--- run_res18.sh
+++ run_res18.sh
@@ -1,7 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python -u main.py           /home/zhanzheng/places365_standard \
-							--resume /home/zhanzheng/Automatic-Image-Colorization/checkpoints/model_best.pth.tar\
 							--batch-size 256 \
 							-j 16 \
 							--optmzr sgd \
