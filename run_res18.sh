9f990d1c69c50e9117a8fcd8681e9e29446b0e5f
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-17 19:09:23 -0500

    Update run_res18.sh

--- run_res18.sh
+++ run_res18.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python -u main.py           /home/zhanzheng/places365_standard \
-							--batch-size 128 \
+							--batch-size 256 \
 							-j 16 \
 							--optmzr sgd \
 							--lr 0.0001 \
